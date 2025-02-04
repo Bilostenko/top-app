6ba8fcea4f03a7d69571a750dae6c578f18fa5b7
BILOSTENKO_D <bilostenkod@gmail.com>
2025-02-04 17:57:05 +0200

    add Htag component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Вказуємо потрібні ваги
+  style: ["normal", "italic"], // Вказуємо стилі
 });
 
 export const metadata: Metadata = {
@@ -25,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={` ${notoSans.variable}`}>
         {children}
       </body>
     </html>
---
+++ components/Htag/Htag.props.ts
@@ -0,0 +1,5 @@
+
+export interface HtagProps extends React.HTMLAttributes<HTMLHeadingElement> {
+    tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    // children: React.ReactNode;
+} 
